b24e4551d0c0018a735260f5e2d4ed8a480dd91c
JianShang <1203698715@qq.com>
2020-05-06 17:55:03 +0800

    添加：五一假期写的

---
+++ 减治法/JZ_分糖果.cpp
@@ -0,0 +1,38 @@
+﻿// 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+// 分治法求分糖果问题
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int getCount(int candy, int plate)
+{
+	int count = 0;
+
+	if (candy < 0)
+	{
+		count = 0;
+	}
+	else if (candy == 1 || plate == 1)
+	{
+		count = 1;
+	}
+	else
+	{
+		count = getCount(candy, plate - 1) + getCount(candy - plate, plate);
+	}
+
+	return count;
+}
+
+int main()
+{
+	int candy, plate;
+	int count;
+
+	cin >> candy >> plate;
+	count = getCount(candy, plate);
+	cout << count << endl;
+
+	return 0;
+}
---
+++ 减治法/JZ_满二叉树任意两节点的最近共同祖先.cpp
@@ -0,0 +1,38 @@
+﻿// 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+// 分治法求满二叉树任意两个节点的最近共同祖先(nearest common ancestor)
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int findNCA(int x, int y)
+{
+	int result;
+
+	if (x == y)
+	{
+		result = x;
+	}
+	else if (x > y)
+	{
+		result = findNCA(x / 2, y);
+	}
+	else
+	{
+		result = findNCA(x, y / 2);
+	}
+
+	return result;
+}
+
+int main()
+{
+	int x, y;
+	int nca;
+
+	cin >> x >> y;
+	nca = findNCA(x, y);
+	cout << nca << endl;
+
+	return 0;
+}
